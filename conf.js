7eb3decb1c45e02cec2ab3f2f9c8965083ec90ff
JCornflower <ol2007july@gmail.com>
2019-01-03 13:43:54 +0200

    PASSED

--- conf.js
+++ conf.js
@@ -5,7 +5,9 @@
     },
     specs: ['spec.js'],
    framework: 'jasmine2' ,
-   //getPageTimeout: 30000,
+   jasmineNodeOpts: {
+   defaultTimeoutInterval: 30000
+   },
     onPrepare: function() {
         var jasmineReporters = require('C:/Users/user/Documents/GitHub/ProtractorTest/node_modules/jasmine-reporters');
         jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter(null, true, true)
--- spec.js
+++ spec.js
@@ -6,26 +6,32 @@
    expect(guru.getText()).toEqual('Hello GURU99!');
      });
    });*/
+var el = element(by.xpath('/html/body/gyr-root/div/gyr-main/ng-component/div[2]'));
+
+waitUntilReady = function (el) {
+  browser.wait(function () {
+      return el.isPresent();
+  },10000);
+  browser.wait(function () {
+      return el.isDisplayed();
+  },10000);
+};
 
 
 describe('Rydoo title', function() {
   it('should have a title', function() {
     browser.waitForAngularEnabled(false);
     browser.get('https://staging-app.rydoo.com/');
-    //var user = element(by.id('username'));
-  //expect((user).isPresent()).toBeTruthy(); 
-  //});
-  //  browser.wait(function() {
-  //     return element(by.id('username')).isPresent();
-  //  }, 5000);
     element(by.id('username')).sendKeys('testeraffa16@gmail.com');
     element(by.id('nextButton')).click();
     browser.pause();
+    //browser.wait(element(by.id('nextButton')));
     expect(element(by.id('password')).isDisplayed());
     expect(element(by.id('loginButton')).isDisplayed());
     element(by.id('password')).sendKeys('test1605');
     element(by.id('loginButton')).click();
-    browser.sleep(30000);
+    waitUntilReady(el);
+    //browser.wait(element(by.xpath('/html/body/gyr-root/div/gyr-main/ng-component/div[2]'), 10000));
     expect(browser.getCurrentUrl()).toEqual('https://staging-app.rydoo.com/dashboard');
   });
 });
